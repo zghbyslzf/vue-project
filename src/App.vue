df201dbea90fc89a3b2370efd953c82436358a14
Maya <zghbyslzf@163.com>
2019-07-16 10:16:31 +0800

    添加项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/styles/normalize.scss";
+html,
+body {
+  @include vw();
+}
+.app {
+  @include flexCenter();
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div class="title">测测你的安全意识有多强</div>
+    <div class="tips">你的人生稳如泰山还是危机四伏</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  max-width: 750px;
+  width: 100%;
+  font-size: 0.24rem;
+  text-align: center;
+
+  .title {
+    @extend .MARGIN_TOP_20;
+    font-size: 0.4rem;
+  }
+
+  .tips {
+    @extend .MARGIN_TOP_20;
+    color: $c_66;
+  }
+}
+</style>
